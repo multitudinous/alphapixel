47266d870d105a4f0db33e08013f21644f5cc4dc
Chris 'Xenon' Hanson <xenon@alphapixel.com>
2014-11-18 15:19:11 -0700

    Switch to VTP's FindGDAL.cmake
    VTP's FindGDAL.cmake has an option to specify a debug version of the
    library as well. Taken from:
    
    https://code.google.com/p/vtp/source/browse/trunk/CMakeModules/FindGDAL.cmake?r=7463

--- LandsatLook/CMakeModules/FindGDAL.cmake
+++ LandsatLook/CMakeModules/FindGDAL.cmake
@@ -1,6 +1,6 @@
 # Locate gdal
 # This module defines
-# GDAL_LIBRARY
+# GDAL_LIBRARIES
 # GDAL_FOUND, if false, do not try to link to gdal 
 # GDAL_INCLUDE_DIR, where to find the headers
 #
@@ -19,20 +19,22 @@
 # This makes the presumption that you are include gdal.h like
 # #include "gdal.h"
 
+set(GDAL_FIND_DEBUG TRUE CACHE BOOL "Also search for the debug version of the GDAL library")
+
+if(GDAL_FIND_DEBUG)
+	if(GDAL_INCLUDE_DIR AND GDAL_LIBRARY AND GDAL_LIBRARY_DEBUG)
+		set(GDAL_FIND_QUIETLY TRUE)
+	endif(GDAL_INCLUDE_DIR AND GDAL_LIBRARY AND GDAL_LIBRARY_DEBUG)
+else(GDAL_FIND_DEBUG)
+	if(GDAL_INCLUDE_DIR AND GDAL_LIBRARY)
+		set(GDAL_FIND_QUIETLY TRUE)
+	endif(GDAL_INCLUDE_DIR AND GDAL_LIBRARY)
+endif(GDAL_FIND_DEBUG)
+
 FIND_PATH(GDAL_INCLUDE_DIR gdal.h
-  PATHS
+  HINTS
   $ENV{GDAL_DIR}
-  NO_DEFAULT_PATH
-    PATH_SUFFIXES include
-)
-
-FIND_PATH(GDAL_INCLUDE_DIR gdal.h
-    PATHS ${CMAKE_PREFIX_PATH} # Unofficial: We are proposing this.
-    NO_DEFAULT_PATH
-    PATH_SUFFIXES include
-)
-
-FIND_PATH(GDAL_INCLUDE_DIR gdal.h
+  PATH_SUFFIXES include
   PATHS
   ~/Library/Frameworks/gdal.framework/Headers
   /Library/Frameworks/gdal.framework/Headers
@@ -57,20 +59,10 @@ FIND_PATH(GDAL_INCLUDE_DIR gdal.h
 )
 
 FIND_LIBRARY(GDAL_LIBRARY 
-  NAMES gdal gdal_i gdal1.7.0 gdal1.6.0 gdal1.5.0 gdal1.4.0 gdal1.3.2 GDAL
-  PATHS
+  NAMES gdal GDAL
+  HINTS
   $ENV{GDAL_DIR}
-  NO_DEFAULT_PATH
   PATH_SUFFIXES lib64 lib
-)
-FIND_LIBRARY(GDAL_LIBRARY 
-  NAMES gdal gdal_i gdal1.7.0 gdal1.6.0 gdal1.5.0 gdal1.4.0 gdal1.3.2 GDAL
-  PATHS ${CMAKE_PREFIX_PATH} # Unofficial: We are proposing this.
-    NO_DEFAULT_PATH
-    PATH_SUFFIXES lib64 lib
-)
-FIND_LIBRARY(GDAL_LIBRARY 
-  NAMES gdal gdal_i gdal1.7.0 gdal1.6.0 gdal1.5.0 gdal1.4.0 gdal1.3.2 GDAL
   PATHS
     ~/Library/Frameworks
     /Library/Frameworks
@@ -82,13 +74,38 @@ FIND_LIBRARY(GDAL_LIBRARY
     /opt
     /usr/freeware
     [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;GDAL_ROOT]/lib
-  PATH_SUFFIXES lib64 lib
 )
 
-SET(GDAL_FOUND "NO")
-IF(GDAL_LIBRARY AND GDAL_INCLUDE_DIR)
-  SET(GDAL_FOUND "YES")
-ENDIF(GDAL_LIBRARY AND GDAL_INCLUDE_DIR)
+FIND_LIBRARY(GDAL_LIBRARY_DEBUG 
+  NAMES gdald GDALd
+  HINTS
+  $ENV{GDAL_DIR}
+  PATH_SUFFIXES lib64 lib
+  PATHS
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /sw
+    /opt/local
+    /opt/csw
+    /opt
+    /usr/freeware
+    [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;GDAL_ROOT]/lib
+)
 
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GDAL DEFAULT_MSG GDAL_INCLUDE_DIR GDAL_LIBRARY)
 
-
+if(GDAL_FOUND)
+	if(GDAL_FIND_DEBUG)
+		if(NOT GDAL_LIBRARY_DEBUG)
+			set(GDAL_LIBRARY_DEBUG ${GDAL_LIBRARY})
+		endif(NOT GDAL_LIBRARY_DEBUG)
+		set(GDAL_LIBRARIES debug ${GDAL_LIBRARY_DEBUG} optimized ${GDAL_LIBRARY})
+	else(GDAL_FIND_DEBUG)
+		set(GDAL_LIBRARIES ${GDAL_LIBRARY})
+	endif(GDAL_FIND_DEBUG)
+else(GDAL_FOUND)
+	SET(GDAL_LIBRARIES )
+endif(GDAL_FOUND)
